160d4007fc974df5b1f61fad913d6e53d5ca6181
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-01-18 00:09:59 +0100

    Update Header.js

--- src/components/loginAndCreateAccount/Header.js
+++ src/components/loginAndCreateAccount/Header.js
@@ -3,6 +3,8 @@ const Header = () => {
       <p>Welcome back</p>
       <p>West</p>
       <p>Current balance</p>
+      <h2>#250000000</h2>
+      <p>Wednesday, 12th December</p>
    </div>
 };
 export default Header;
