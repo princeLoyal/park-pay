7e0d18c9bb48489471963e6802dac13ec649b0b9
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-01-19 19:44:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import FirstPage from './components/loginAndCreateAccount/FirstPage';
 import Header from './components/loginAndCreateAccount/Header';
-import Transaction from './components/loginAndCreateAccount/Login';
+import Transaction from './components/loginAndCreateAccount/Transaction';
 import classes from './App.module.css';
 
 function App() {
