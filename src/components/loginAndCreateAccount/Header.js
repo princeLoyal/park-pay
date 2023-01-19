60ec374af08bd605b7ec799037d2f8063c88725b
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-01-19 16:54:32 +0100

    Update Header.js

--- src/components/loginAndCreateAccount/Header.js
+++ src/components/loginAndCreateAccount/Header.js
@@ -1,8 +1,5 @@
 import { Fragment } from "react";
 import classes from './Header.module.css';
-import wavyHand from '../../picturesAndFiles/mdi_hand-wave-outline.png';
-import hiddenEye from '../../picturesAndFiles/Eye\,\ Hidden.png'
-import notificationBell from '../../picturesAndFiles/clarity_notification-outline-badged.png';
 const Header = () => {
     const weekDays = ['Monday', 'Tuesday', 'Wednessday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     const months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -18,13 +15,13 @@ const Header = () => {
                 <div className={classes['header-firstDiv']}>
                     <p>Welcome back,</p> 
                     <p>West</p> 
-                    <img src={wavyHand} width='20' className={classes['header-wavyhand']}/>
-                    <img src={notificationBell} width='40' className={classes['header-bell']}/>
+                    <img src='' width='20' className={classes['header-wavyhand']}/>
+                    <img src='' width='40' className={classes['header-bell']}/>
                 </div>
             <div className={classes['header-secondDiv']}>
                 <p>Current balance</p>
                 <p> &#8358;{date.currentBalance}.00
-                <span><img src={hiddenEye} alt='A hidden eye' width='25'/></span>
+                <span><img src='' alt='A hidden eye' width='25'/></span>
                 </p>
                 <p>{date.weekDay}, {date.monthDay}th {date.month}</p>
             </div>
